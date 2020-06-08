d9d3c1a72b7acddb27a0b6ba9f8e43458111a0cf
Devesh puri goswami <weareoutsider1@outlook.com>
2020-06-09 02:22:43 +0530

    Added mvc to application

--- WebGentle.BookStore7/WebGentle.BookStore7/Startup.cs
+++ WebGentle.BookStore7/WebGentle.BookStore7/Startup.cs
@@ -16,9 +16,10 @@ namespace WebGentle.BookStore7
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to confi gure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,15 +27,16 @@ namespace WebGentle.BookStore7
                 app.UseDeveloperExceptionPage();
             }
 
+
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
+           
         }
     }
 }
